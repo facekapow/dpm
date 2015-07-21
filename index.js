54ec9cbd49e1337de86d716c6381c62d3c213698
Ariel Abreu <facekapow2002@gmail.com>
2015-07-21 10:10:13 -0400

    dpm v0.0.1

---
+++ index.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+var args = process.argv.slice(2);
+var http = require('http');
+var fs = require('fs');
+var unzip = require('unzip');
+
+if (!args[0]) {
+  console.log('why no args?');
+  return console.log('see ya.');
+}
+
+if (args[0] === 'install') {
+  if (!args[1]) {
+    return console.log('Need something to install!');
+  }
+
+  fs.stat('apps', function(err) {
+    if (err) {
+      return console.log('no \'apps\' folder found. are you sure you\'re in the right place? anyway, bye.');
+    }
+
+    var json_content = '';
+    http.get('https://raw.githubusercontent.com/ArielAbreu/desktopjs-apps/master/apps.json', function(res) {
+      res.on('data', function(data) {
+       json_content += data;
+      }).on('end', function() {
+        var json_obj = JSON.parse(json_content);
+        try {
+          var obj = json_obj[args[1]];
+          var file = fs.createWriteStream('apps/' + obj.tar);
+          http.get('https://raw.githubusercontent.com/ArielAbreu/desktopjs-apps/master/apps/' + obj.tar, function(res) {
+            res.on('data', function(data) {
+              file.write(data);
+            }).on('end', function() {
+              file.end();
+              fs.createReadStream('apps/' + obj.tar).pipe(unzip.Extract({ path: 'apps/' + args[1] }));
+            });
+          });
+        } catch(e) {
+          return console.log('nothing here.');
+        }
+      });
+    });
+  });
+}
